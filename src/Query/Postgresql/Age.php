7e88e256df231a729a06a508b30e147c6610d1c0
shyam <shyam@ghanshyamdigital.com>
2021-06-01 23:50:57 +0530

    Add postgres `age` function and tests.

---
+++ src/Query/Postgresql/Age.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace DoctrineExtensions\Query\Postgresql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\QueryException;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * Class Age
+ * @package DoctrineExtensions\Query\Postgresql
+ */
+class Age extends FunctionNode
+{
+    public $date1 = null;
+
+    public $date2 = null;
+
+    /**
+     * @override
+     * @param SqlWalker $sqlWalker
+     * @return string
+     */
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return 'AGE(' . $this->date1->dispatch($sqlWalker) . ', ' . $this->date2->dispatch($sqlWalker) . ')';
+    }
+
+    /**
+     * @override
+     * @param Parser $parser
+     * @throws QueryException
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->date1 = $parser->ArithmeticPrimary();
+
+        $lexer = $parser->getLexer();
+        if ($lexer->isNextToken(Lexer::T_COMMA)) {
+            $parser->match(Lexer::T_COMMA);
+            $this->date2 = $parser->ArithmeticPrimary();
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
---
+++ tests/Query/Postgresql/AgeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DoctrineExtensions\Tests\Query\Postgresql;
+
+use DoctrineExtensions\Tests\Query\PostgresqlTestCase;
+
+class AgeTest extends PostgresqlTestCase
+{
+    public function testAge()
+    {
+        $this->assertDqlProducesSql(
+            "SELECT age('2012-03-05') from DoctrineExtensions\Tests\Entities\Blank b",
+            "SELECT age('2012-03-05') AS sclr_0 FROM Blank b0_"
+        );
+    }
+
+    public function testAgeWithTwoParameters()
+    {
+        $this->assertDqlProducesSql(
+            "SELECT age('2012-03-05', '2010-04-01') from DoctrineExtensions\Tests\Entities\Blank b",
+            "SELECT age('2012-03-05', '2010-04-01') AS sclr_0 FROM Blank b0_"
+        );
+    }
+}
