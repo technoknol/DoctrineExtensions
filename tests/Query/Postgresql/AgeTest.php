c5d23c982067841122a9e7fc21e381c1fbb48a76
shyam <shyam@ghanshyamdigital.com>
2021-06-01 23:54:30 +0530

    fix postgres `age` function test.

--- tests/Query/Postgresql/AgeTest.php
+++ tests/Query/Postgresql/AgeTest.php
@@ -9,16 +9,16 @@ class AgeTest extends PostgresqlTestCase
     public function testAge()
     {
         $this->assertDqlProducesSql(
-            "SELECT age('2012-03-05') from DoctrineExtensions\Tests\Entities\Blank b",
-            "SELECT age('2012-03-05') AS sclr_0 FROM Blank b0_"
+            "SELECT AGE('2012-03-05') from DoctrineExtensions\Tests\Entities\Blank b",
+            "SELECT AGE('2012-03-05') AS sclr_0 FROM Blank b0_"
         );
     }
 
     public function testAgeWithTwoParameters()
     {
         $this->assertDqlProducesSql(
-            "SELECT age('2012-03-05', '2010-04-01') from DoctrineExtensions\Tests\Entities\Blank b",
-            "SELECT age('2012-03-05', '2010-04-01') AS sclr_0 FROM Blank b0_"
+            "SELECT AGE('2012-03-05', '2010-04-01') from DoctrineExtensions\Tests\Entities\Blank b",
+            "SELECT AGE('2012-03-05', '2010-04-01') AS sclr_0 FROM Blank b0_"
         );
     }
 }
